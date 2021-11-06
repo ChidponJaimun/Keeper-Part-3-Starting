3b5afbd4a16ffc1fd414030e8db39fe4bbd2dd46
ChidponJaimun <61172207+ChidponJaimun@users.noreply.github.com>
2021-11-07 01:36:24 +0700

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
-  const [note, setNote] = useState([]);
+  const [allNote, setAllNote] = useState([]);
 
   return (
     <div>
